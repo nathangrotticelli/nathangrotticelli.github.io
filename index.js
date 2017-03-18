2ffadbf75a1515dd37969b99b167f97f4b05a350
nathangrotticelli <nathangrotticelli@gmail.com>
2017-03-18 18:44:56 -0400

    yt update9

--- index.js
+++ index.js
@@ -33,7 +33,22 @@ function onLoad() {
     $('.ytplayer-frame').attr('src', 'https://www.youtube.com/embed/5xqPUyI3pQY');
   // playButton = document.querySelector('#toggleplay');
   // muteButton = document.querySelector('#togglemute');
+  $('.video').on('click', function( e ) {
+    console.log('i am video');
+    window.alert('i am video');
+});
+$('.ytplayer-frame').on('click', function( e ) {
+    console.log('i am player frame');
+    window.alert('i am lpayer frame');
+});
+     $('iframe').on('click', function( e ) {
+        console.log('hi im here');
+        alert('hi im here');
 
+       // e.preventDefault();
+       //assumes that by forcing window to go to youtube will kick up option to open in app where experience works - plays my full showreel
+       // window.location = "https://www.youtube.com/watch?v=0x16ngo8xfY&list=PLzSXIFcDqpiCiKXMtXtVIHnmor9uUsEhC&autoplay=1";
+     });
   // playButton.addEventListener('click', onTogglePlay);
   // muteButton.addEventListener('click', onToggleMute);
 }
@@ -74,22 +89,7 @@ $("#contact").click(function() {
         scrollTop: $(".expanded-contact-form").offset().top},
         'slow');
 });
-$('.video').on('click', function( e ) {
-    console.log('i am video');
-    window.alert('i am video');
-});
-$('.ytplayer-frame').on('click', function( e ) {
-    console.log('i am player frame');
-    window.alert('i am lpayer frame');
-});
-     $('iframe').on('click', function( e ) {
-        console.log('hi im here');
-        window.alert('hi im here');
 
-       // e.preventDefault();
-       //assumes that by forcing window to go to youtube will kick up option to open in app where experience works - plays my full showreel
-       // window.location = "https://www.youtube.com/watch?v=0x16ngo8xfY&list=PLzSXIFcDqpiCiKXMtXtVIHnmor9uUsEhC&autoplay=1";
-     });
 
 $(".white-text").click(function() {
     $('html,body').animate({
